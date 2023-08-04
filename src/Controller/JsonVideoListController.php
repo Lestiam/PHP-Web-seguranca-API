7ccdaef8708e557e86dad14d88746a8d856cfc2a
IgorTelesCaetano <igor-t-c@hotmail.com>
2023-08-04 15:59:01 -0300

    Conhecemos mais o conceito de APIs e o que elas são na prática; Aprendemos a manipular JSON com PHP usando as funções json_encode e json_decode; Criamos nossa primeira API com PHP usando JSON como formato de transferência de dados; Conhecemos referências sobre tokens, Postman e mais detalhes sobre criação de APIs.

---
+++ src/Controller/JsonVideoListController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alura\Mvc\Controller;
+
+use Alura\Mvc\Entity\Video;
+use Alura\Mvc\Repository\VideoRepository;
+
+class JsonVideoListController implements Controller
+{
+    public function __construct(private VideoRepository $videoRepository)
+    {
+    }
+
+    public function processaRequisicao(): void
+    {
+        $videoList = array_map(function (Video $video) : array {
+            return [
+                'url' => $video->url,
+                'title' => $video->title,
+                'file_path' => '/img/uploads/' . $video->getFilePath(),
+            ];
+        }, $this->videoRepository->all()); //pega todos os vídeos
+        echo json_encode($videoList); //pega algum valor do php e o transforma para uma string em json
+    }
+}
